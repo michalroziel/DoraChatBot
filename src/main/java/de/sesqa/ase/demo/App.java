b6c1721e05a9385ea72b3854bdce6600bb9d1c7f
Michał Roziel <miro00009@htwsaar.de>
2025-05-13 16:43:29 +0200

    removing tracked target files + fixing error in main method

--- src/main/java/de/sesqa/ase/demo/App.java
+++ src/main/java/de/sesqa/ase/demo/App.java
@@ -28,7 +28,7 @@ package de.sesqa.ase.demo;
             int array1[] = {1, 2, 3};
             int array2[] = {4, 5, 6};
 
-            /****
+            /*
              * Fix: Use Arrays.equals() to compare the contents of the arrays
              * <p>Arrays.equals(array1, array2) returns true if the two arrays are equal in length and contain the same elements in the same order.</p>
              * instead of using the equals method directly on the array reference.
