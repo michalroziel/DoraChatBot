0ebfe678af95bcada94f784386783aec475bc568
Yannic Hock <yaho00002@htwsaar.de>
2025-06-20 12:35:25 +0200

    Error Prone
    Errorprone wurde als Hilfmittel der statischen Codeanalyse hinzugefügt

--- src/main/java/de/sesqa/ase/DoraChatBot.java
+++ src/main/java/de/sesqa/ase/DoraChatBot.java
@@ -2,6 +2,8 @@ package de.sesqa.ase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+
 @SpringBootApplication
 public class DoraChatBot {
     public static void main(String[] args) {
