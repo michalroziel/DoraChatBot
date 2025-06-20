c2ffd998d0770fb36e578808bf07a92cef5ab751
Yannic Hock <yaho00002@htwsaar.de>
2025-06-21 01:10:04 +0200

    Showcase Versioning on examplepage

--- src/main/java/de/sesqa/ase/BaseController.java
+++ src/main/java/de/sesqa/ase/BaseController.java
@@ -1,20 +1,23 @@
 package de.sesqa.ase;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class VersionController {
-
+@Controller
+public class BaseController {
     @Value("${version:unknown}")
     private String version;
 
     @Value("${buildHash:unknown}")
     private String buildNumber;
 
-    @GetMapping("/version")
-    public String getVersion() {
-        return "Version: " + version + ", Commit: " + buildNumber;
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("version", version);
+        model.addAttribute("buildNumber", buildNumber);
+        return "index";
     }
 }
