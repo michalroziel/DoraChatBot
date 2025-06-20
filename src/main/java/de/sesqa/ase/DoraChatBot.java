b99df93a9a5ffe4957a7f5243d8e617ef1b5aa95
Yannic Hock <yaho00002@htwsaar.de>
2025-06-20 13:39:04 +0200

    Maven Site JUnit & Sportbugs
    Einbindung von Surefire/JUnit reports und von Spotbugs in die Maven Site

--- src/main/java/de/sesqa/ase/DoraChatBot.java
+++ src/main/java/de/sesqa/ase/DoraChatBot.java
@@ -2,7 +2,6 @@ package de.sesqa.ase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
 
 @SpringBootApplication
 public class DoraChatBot {
