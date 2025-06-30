455bc9dbd498a0df007dd3aec0362c75ad285e59
Yannic Hock <yaho00002@htwsaar.de>
2025-06-30 21:08:52 +0200

    Spotbug fixes

--- src/main/java/de/sesqa/ase/metrics/CollectdClient.java
+++ src/main/java/de/sesqa/ase/metrics/CollectdClient.java
@@ -3,6 +3,7 @@ package de.sesqa.ase.metrics;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.nio.charset.StandardCharsets;
 
 public class CollectdClient {
     private final String host;
@@ -22,9 +23,9 @@ public class CollectdClient {
 
     public void sendMetric(String typeInstance, String type, long value) {
         try (DatagramSocket socket = new DatagramSocket()) {
-            String metric = String.format("PUTVAL \"%s/%s-%s/%s\" interval=10 N:%d\n",
+            String metric = String.format("PUTVAL \"%s/%s-%s/%s\" interval=10 N:%d%n",
                     InetAddress.getLocalHost().getHostName(), "DoraChatBot", typeInstance, type, value);
-            byte[] data = metric.getBytes();
+            byte[] data = metric.getBytes(StandardCharsets.UTF_8);
             DatagramPacket packet = new DatagramPacket(data, data.length, InetAddress.getByName(host), port);
             socket.send(packet);
         } catch (Exception e) {
